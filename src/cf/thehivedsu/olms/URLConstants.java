d405f203a3ccac9170844188f88cda24eea88665
Abhishek Munie <abhishekmunie@yahoo.co.in>
2015-10-27 13:48:38 +0530

    Adds missed URLConstants

---
+++ src/cf/thehivedsu/olms/URLConstants.java
@@ -0,0 +1,30 @@
+package cf.thehivedsu.olms;
+
+public class URLConstants {
+
+	// Auth URLs
+
+	public static final String SIGNIN = "/auth/signin/";
+	public static final String SIGNOUT = "/auth/signout/";
+
+	public static final String ADMIN_AUTH_REQUIRED = "/auth/signin/admin/";
+	public static final String EMPLOYEE_AUTH_REQUIRED = "/auth/signin/employee/";
+	public static final String MANAGER_AUTH_REQUIRED = "/auth/signin/manager/";
+
+	public static final String DASHBOARD = "/dashboard/";
+
+	// Admin URLs
+
+	public static final String ADMIN_PORTAL = "/admin/";
+
+	// Employee URLs
+
+	public static final String EMPLOYEE_PORTAL = "/employee/";
+
+	// Manager URLs
+
+	public static final String MANAGER_PORTAL = "/manager/";
+
+	public static final String INIT_SQL_FILE = "/WEB-INF/init.sql";
+
+}
